27d6acf9dfd26f051aaf77f553185910921ccfb2
Jansxn <109086403+Jansxn@users.noreply.github.com>
2022-09-13 18:31:02 +0530

    Code updated to reduce lag
    Started using mouse library instead of autopygui to reduce lag

--- AIVirtualMouse.py
+++ AIVirtualMouse.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import time
-import pyautogui
+import mouse
 import mediapipe as mp
 import math
 
@@ -97,22 +97,26 @@ pTime = 0
 plocX, plocY = 0, 0
 clocX, clocY = 0, 0
 cap = cv2.VideoCapture(0)
-stop =0
+stop = 0
 cap.set(3, wCam)
 cap.set(4, hCam)
 wScr, hScr = 1280, 700
+text = ''
+close = 0
 
 detector = handDetector()
-wScr, hScr = pyautogui.size()
+#
 
 # print(wScr, hScr)
+
+frame_rate = 10
+prev = 0
 
 while True:
     # Step1: Find the landmarks
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
-
     # Step2: Get the tip of the index and middle finger
     if len(lmList) != 0:
         x0, y0 = lmList[4][1:]
@@ -120,82 +124,72 @@ while True:
         x2, y2 = lmList[12][1:]
         x3, y3 = lmList[16][1:]
         x4, y4 = lmList[20][1:]
-        
         # Step3: Check which fingers are up
         fingers = detector.fingersUp()
         cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
                       (255, 0, 255), 2)
-
         # Step4: Only Index Finger: Moving Mode
         if fingers[1] == 1 and fingers[2] == 0:
-
             # Step5: Convert the coordinates
             xi = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
             yi = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
-
             # Step6: Smooth Values
             clocX = plocX + (xi - plocX) / smoothening
             clocY = plocY + (yi - plocY) / smoothening
-
             # Step7: Move Mouse
-            pyautogui.moveTo(wScr - clocX, clocY)
+            mouse.move(wScr - clocX, clocY)
             cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
             plocX, plocY = clocX, clocY
-        
         if fingers[1] == 0 and fingers [2] == 0 and fingers[4]:
             stop+=1
             if stop>20:
-                pyautogui.click(button ='right')
+                mouse.right_click()
                 stop = 0
-
         # Step8: Both Index and middle are up: Clicking Mode
         if fingers[1] == 1 and fingers[2] == 1:
-
             # Step9: Find distance between fingers
             length, img, lineInfo = detector.findDistance(8, 12, img)
             stop+=1
-
             # Step10: Click mouse if distance short
             if length < 40 and stop>20:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
-                pyautogui.click()
+                mouse.click(button='left')
                 stop = 0
-            elif length>40:
-                xi = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
-                yi = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
-    
-                # Step6: Smooth Values
-                clocX = plocX + (xi - plocX) / smoothening
-                clocY = plocY + (yi - plocY) / smoothening
-    
-                plocX, plocY = clocX, clocY
-                pyautogui.dragTo(wScr - clocX, clocY)
-                stop = 0
-        
+           #elif length>40:
+           #    ##FOR DRAGGING WHILE USING A LOCAL CANVAS APP
+           #    xi = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
+           #    yi = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
+           #    # Step6: Smooth Values
+           #    clocX = plocX + (xi - plocX) / smoothening
+           #    clocY = plocY + (yi - plocY) / smoothening
+           #    plocX, plocY = clocX, clocY
+           #    pyautogui.dragTo(wScr - clocX, clocY)
+           #    stop = 0
         if fingers[1] and fingers[2] and fingers[3]:
             stop+=1
             if stop>20:
-                pyautogui.doubleClick()
+                mouse.double_click(button='left')
                 stop = 0
-
         if all(x == 0 for x in fingers):
             if (y1>=(hCam/2)):
-                pyautogui.scroll(-20)
+                mouse.wheel(delta=-1)
             else:
-                pyautogui.scroll(20)
-        
-
-    # Step11: Frame rate
+                mouse.wheel(delta=1)
+        # ALT + F4 to clopse app    
+        #if all(x == 1 for x in fingers):
+        #    text = "Keep fingers up to confirm"
+        #    close += 1
+        #    if close >= 50:
+        #        pyautogui.hotkey('alt', 'f4')
+        #        close=0
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
-
     # Step12: Display
     img1 = cv2.flip(img, 1)
-    cv2.putText(img1, str(int(fps)), (28, 58), cv2.FONT_HERSHEY_PLAIN, 3, (255, 8, 8), 3)
+    cv2.putText(img1, str(fps), (28, 58), cv2.FONT_HERSHEY_PLAIN, 3, (255, 8, 8), 3)
     cv2.imshow("Image", img1)
-    cv2.waitKey(1)
     if (cv2.waitKey(1) & 0xFF == ord('d')):
-        break
+            break
 cap.release()
 cv2.destroyAllWindows()
